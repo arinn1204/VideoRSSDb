b7325ac9bc86e99f79e929f4fb7c26a5aa242003
Aaron Rinn <arinn1250@gmail.com>
2019-12-17 06:20:23 -0800

    changed id from UUID to varchar

--- src/VideoRSSDb/Tables/video_rss.sql
+++ src/VideoRSSDb/Tables/video_rss.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [rss].[video_rss](
 	[id] INT IDENTITY(1,1),
-	[torrent_id] UNIQUEIDENTIFIER NOT NULL,
+	[torrent_id] VARCHAR(64) NOT NULL,
 	[torrent_name] VARCHAR(512) NOT NULL,
 	[time_added] DATETIME2(7) NOT NULL,
 	[magnet] VARCHAR(1024) NOT NULL
